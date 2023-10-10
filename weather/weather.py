4d30f3c5d17b325ec8a207ef076cb26c6004dc5f
udayslathia16 <127138307+udayslathia16@users.noreply.github.com>
2023-10-11 00:36:48 +0530

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -6,7 +6,7 @@ import requests
 
 city=input("Enter the name of the city\n")
 #speaker = win32com.client.Dispatch("SAPI.SpVoice")
-url=f"https://api.weatherapi.com/v1/current.json?key=2e33fd25c43948aeb60180137230904&q={city}&aqi=no"
+url=f"https://api.weatherapi.com/v1/current.json?key="YOUR_API_KEY"={city}&aqi=no"
 
 r=requests.get(url)
 # print(r.text)
